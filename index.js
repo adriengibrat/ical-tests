816548f36fe36982c402dc1ff6742ce7486d8bde
Adrien Gibrat <adrien.gibrat@gmail.com>
2018-06-03 23:48:50 +0200

    wip

--- index.js
+++ index.js
@@ -4,18 +4,29 @@ const { URL, parse } = require('url')
 const mailer = require('nodemailer')
 const ical = require('ical-generator')
 
-const { NOW_URL, GMAIL_USER, GMAIL_PASSWORD } = process.env
+const { NOW_URL, SENDGRID_USERNAME, SENDGRID_PASSWORD } = process.env
 const summary = 'Example Event'
-const htmlDescription = '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 3.2//EN"><html><body><em>It</em> <b>works</b> ;)</body></html>'
+const htmlDescription = '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 3.2//EN"><html><body><img alt="calendar" src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e9/Google_Calendar.png/128px-Google_Calendar.png"><em>It</em> <b>works</b> ;)</body></html>'
 const attentees = [ 'adrien.gibrat@gmail.com', 'a.gibrat@oodrive.com' ]
 const method = 'REQUEST'
 const filename = 'calendar.ics'
 let messageId
 
+// const smtp = mailer.createTransport({
+//     service: 'SendGrid',
+//     auth: { user: SENDGRID_USERNAME, pass: SENDGRID_PASSWORD },
+// })
+
 const smtp = mailer.createTransport({
-    service: 'gmail',
-    auth: { user: GMAIL_USER, pass: GMAIL_PASSWORD },
+    host: 'mail.oodrive.com',
+    port: 587,
+    secure: true,
+    auth: {
+        user: 'a.gibrat@oodrive.com',
+        pass: 'xxx'
+    }
 })
+
 const calendar = ical({
     domain: new URL(NOW_URL).hostname,
     prodId: { company: 'Oodrive', product: 'Boardnox' },
@@ -59,10 +70,15 @@ module.exports = router(
                 name: email.replace(/@.*$/, ''),
             })))
             smtp.sendMail({
-                // from: { name: 'Adrien', address: 'adrien.gibrat+sender@gmail.com' },
-                sender: { name: 'Boardnox', address: 'notifications@oodrive.com' }, // sender seems broken, need to check gmail rules about DKIM / SPDF
+                envelope: {
+                    from: 'adrien.gibrat+return@gmail.com',
+                    to: attentees.map(email => ({ name: email.replace(/@.*$/, ''), address: email })),
+                },
+                from: { name: 'Adrien From', address: 'adrien.gibrat+from@gmail.com' },
+                sender: { name: 'Adrien Sender', address: 'adrien.gibrat+sender@gmail.com' },
+                replyTo: { name: 'Adrien Reply', address: 'adrien.gibrat+reply@gmail.com' },
                 to: attentees.map(email => ({ name: email.replace(/@.*$/, ''), address: email })),
-                subject: messageId ? `up: ${event.summary()}` : event.summary(),
+                subject: event.summary(),
                 inReplyTo: messageId || undefined,
                 references: messageId ? [messageId] : undefined,
                 alternatives: [
@@ -72,14 +88,13 @@ module.exports = router(
                     },
                     {
                         contentType: 'text/html; charset=utf-8',
-                        content: 'hope <em>this</em> <b>works</b>',
+                        content: 'hope <em>this</em> <b>works</b><img alt="calendar" src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e9/Google_Calendar.png/128px-Google_Calendar.png">',
                     },
                     {
-                        contentType: `text/calendar; charset=utf-8; method=${method}; name=${filename}`,
+                        contentType: `text/calendar; charset=utf-8; method=${method}`, // ; name=${filename}
                         content: calendar.toString()
                             // RSVP not supported yet... see https://github.com/sebbo2002/ical-generator/pull/58
                             .replace(/ATTENDEE/g, 'ATTENDEE;RSVP=FALSE'),
-
                     },
                 ],
             }, (error, message) => error ? reject(error) : resolve(message))
